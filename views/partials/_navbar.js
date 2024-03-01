8294dabdce96edab1bab6ce53d41aca6f83e8af2
Quithan <75689363+Quithan@users.noreply.github.com>
2024-03-01 17:54:17 +0600

    Add files via upload

---
+++ views/partials/_navbar.js
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-center">
+  <a class="navbar-brand" href="/">Sport Store</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/admin">Admin Dashboard</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/my-profile">My Profile</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/sports">games</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/logout">Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
